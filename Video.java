aa38325eb726007bedf743b7cd00a3e5fe0f5c68
Aceoffrederik1 <36229721+Aceoffrederik1@users.noreply.github.com>
2018-12-06 15:16:56 +0100

    Add files via upload

---
+++ Video.java
@@ -0,0 +1,6 @@
+public abstract class Video{
+    public abstract String getName();
+    public abstract String getGenre();
+    public abstract int getYear();
+    public abstract boolean isRrated();
+}
