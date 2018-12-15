3da97e66923b93aa37f10c9012f1c673c1f74464
MichellaJ <45658962+MichellaJ@users.noreply.github.com>
2018-12-15 15:06:50 +0100

    Add files via upload

---
+++ Reader/MovieReader.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.util.Arrays;
+import java.io.*;
+import java.lang.String;
+import java.awt.*;
+
+public class MovieReader
+{
+    private String fileName;
+    public ArrayList<Movie> movies;
+
+    public MovieReader()
+    {
+        this.fileName = "mText.txt";
+        movies = new ArrayList<Movie>();
+    }
+
+    public ArrayList<Movie> read() throws IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        ArrayList<Movie> movies = read(br);
+        br.close();
+        return movies;
+    }
+    
+    public ArrayList<Movie> read(BufferedReader br)
+    {
+        try{
+            String line = br.readLine();
+            while(br.ready() && line != null){
+                line = br.readLine();
+                String[] splitInfo = line.split(";");
+                String title = splitInfo[0].trim();
+                int year = Integer.parseInt(splitInfo[1].trim());
+                String[] genres = splitInfo[2].split(",");
+                for(String g : genres){
+                    String newG = g.trim();
+                    int index = -1;
+                    for (int i=0;i<genres.length;i++) {
+                        if (genres[i].equals(g)) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    genres[index] = newG;
+                    }
+                double rating = Double.parseDouble(splitInfo[3].trim().replace(",","."));
+                
+                Movie m = new Movie(title, year, genres, rating);
+                movies.add(m);
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        return movies;
+    }
+    
+    public void sortAlphbetic(){
+        Collections.sort(movies, Comparator.comparing(Movie::getTitle));
+    }
+    
+    public void sortByYear(){
+        Collections.sort(movies, Comparator.comparing(Movie::getYear));
+    }
+    
+    public ArrayList<Movie> sortByGenre(String genre){
+        ArrayList<Movie> rightGenre = new ArrayList<Movie>();
+        for(Movie m : movies){
+            for(String g : m.getGenre()){
+                if(genre.equals(g)){
+                    rightGenre.add(m);
+                }
+            }
+        }
+        
+        return rightGenre;
+    }
+    
+    public ArrayList<Image> getMoviePictures(){
+        ArrayList<Image> moviePictureFiles = new ArrayList<Image>();
+        for(Movie m : movies){
+            moviePictureFiles.add(m.getPicture());
+        }
+        return moviePictureFiles;
+    }
+}
---
+++ Reader/TVSeriesReader.java
@@ -0,0 +1,113 @@
+import java.util.*;
+import java.util.Arrays;
+import java.io.*;
+import java.lang.String;
+import java.awt.*;
+
+public class TVSeriesReader
+{
+    private String fileName;
+    private ArrayList<TVSerie> series;
+
+    public TVSeriesReader()
+    {
+        this.fileName = "tvText.txt";
+        series = new ArrayList<TVSerie>();
+    }
+
+    public ArrayList<TVSerie> read() throws IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        ArrayList<TVSerie> series = read(br);
+        br.close();
+        return series;
+    }
+    
+    public ArrayList<TVSerie> read(BufferedReader br)
+    {
+        try{
+            String line = br.readLine();
+            while(br.ready() && line != null){
+                line = br.readLine();
+                String[] splitInfo = line.split(";");
+                String title = splitInfo[0].trim();
+                String[] years = null;
+                int startYear = 0;
+                int endYear = 0;
+                if(splitInfo[1].contains("-")){
+                    years = splitInfo[1].split("-");
+                    startYear = Integer.parseInt(years[0].trim());
+                    
+                    if(years.length == 2){
+                        if(years[1] != null){
+                            endYear = Integer.parseInt(years[1]);
+                        }
+                    }
+                } else {
+                    startYear = Integer.parseInt(splitInfo[1].trim());
+                }
+                
+                String[] genres = splitInfo[2].split(",");
+                for(String g : genres){
+                    String newG = g.trim();
+                    int index = -1;
+                    for (int i=0;i<genres.length;i++) {
+                        if (genres[i].equals(g)) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    genres[index] = newG;
+                    }
+                double rating = Double.parseDouble(splitInfo[3].trim().replace(",","."));
+                String[] allSeasons = splitInfo[4].split(",");
+                int[][] seasons = new int[allSeasons.length][];
+                int x = 0;
+                for(String s : allSeasons){
+                    String[] se = allSeasons[x].split("-");
+                    int e = Integer.parseInt(se[1].trim());
+                    seasons[x] = new int[e];
+                    for(int i = 0 ; i < e ; i++){
+                        seasons[x][i] = i+1;
+                    }
+                    x++;
+                }
+                
+                TVSerie t = new TVSerie(title, seasons, startYear, endYear, genres, rating);
+                series.add(t);
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        return series;
+    }
+    
+    public void sortAlphbetic(){
+        Collections.sort(series, Comparator.comparing(TVSerie::getTitle));
+    }
+    
+    public void sortByStartYear(){
+        Collections.sort(series, Comparator.comparing(TVSerie::getStartYear));
+    }
+    
+    public ArrayList<TVSerie> sortByGenre(String genre){
+        ArrayList<TVSerie> rightGenre = new ArrayList<TVSerie>();
+        for(TVSerie t : series){
+            for(String g : t.getGenre()){
+                if(genre.equals(g)){
+                    rightGenre.add(t);
+                }
+            }
+        }
+        
+        return rightGenre;
+    }
+    
+    public ArrayList<Image> getTVPictures(){
+        ArrayList<Image> tvPictureFiles = new ArrayList<Image>();
+        for(TVSerie t : series){
+            tvPictureFiles.add(t.getPicture());
+        }
+        return tvPictureFiles;
+    }
+}
